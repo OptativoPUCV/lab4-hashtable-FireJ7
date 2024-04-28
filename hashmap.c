17601652c4870875a99da95043b5e2ea1fe78f1b
FireJ7 <163463357+FireJ7@users.noreply.github.com>
2024-04-28 03:32:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -63,7 +63,7 @@ void insertMap(HashMap * map, char * key, void * value){
 
 
 void enlarge(HashMap * map) {
-  enlarge_called = 1; //no borrar (testing purposes)
+  //enlarge_called = 1; //no borrar (testing purposes)
   Pair** old = map->buckets;
 
   map->capacity *= 2;
